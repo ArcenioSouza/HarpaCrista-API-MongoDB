faa133016a34d167730c10410fadbdaea895bf61
Arcenio Souza <arcenio_neto@icloud.com>
2022-04-04 15:21:04 -0300

    feat(hinosroute.js): :sparkles: add route to search by id, title and chorus

--- src/routes/hinosRoute.js
+++ src/routes/hinosRoute.js
@@ -7,4 +7,31 @@ router.get("/hinos", (req, res) => {
    res.status(200).json(hinos)
 })
 
+router.get("/hinos/id/:id", (req, res) => {
+   let id = parseInt(req.params.id);
+   let response = hinos.filter(hino => hino.id === id)
+   res.status(200).json(response)
+})
+
+router.get("/hinos/title/:title", (req, res) => {
+   let title = req.params.title;
+   let response = hinos.filter(hino => hino.title === title)
+   res.status(200).json(response)
+})
+
+router.get("/hinos/chorusExcerpt/:chorus", (req, res) => {
+   let chorus = req.params.chorus;
+   hinos.filter((hino) => {
+      let indexHino = hino.chorus.indexOf(chorus)
+      console.log(indexHino)
+      if(indexHino !== -1){
+         return res.status(200).json(hino)
+      }
+      else{
+         return res.status(200).json({msg: 'hino not found'})
+      }
+   })
+   
+})
+
 export default router;
